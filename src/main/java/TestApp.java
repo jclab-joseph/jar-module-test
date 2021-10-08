f7126b8c8e642a24dbf2244e66dcb0e91f6d82d2
jichan <joseph@jc-lab.net>
2021-10-08 11:39:12 +0900

    first commit

---
+++ src/main/java/TestApp.java
@@ -0,0 +1,7 @@
+import com.google.gson.internal.GsonBuildConfig;
+
+public class TestApp {
+    public static void main (String[] args) {
+        System.out.println("Hello world: " + GsonBuildConfig.VERSION);
+    }
+}
